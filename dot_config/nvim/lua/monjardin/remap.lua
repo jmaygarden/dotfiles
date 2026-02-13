61d0fa73c09c65adbc90988367196932fbe6e84a
Judge Maygarden <j.maygarden@samsung.com>
2026-02-13 20:24:19 +0000

    sync tmux settings

--- dot_config/nvim/lua/monjardin/remap.lua
+++ dot_config/nvim/lua/monjardin/remap.lua
@@ -1,10 +1,7 @@
 -- ~/.config/nvim/lua/monjardin/remap.lua
 
 local bufnr = vim.api.nvim_get_current_buf()
-vim.keymap.set("n", "<leader>a", function()
-	vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
-	-- or vim.lsp.buf.codeAction() if you don't want grouping.
-end, { silent = true, buffer = bufnr })
+
 vim.keymap.set(
 	"n",
 	"K", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
@@ -13,8 +10,3 @@ vim.keymap.set(
 	end,
 	{ silent = true, buffer = bufnr }
 )
-
-vim.keymap.set("n", "<leader>C", function()
-	vim.api.nvim_win_close(0, false)
-	vim.cmd.Cargo("check")
-end)
